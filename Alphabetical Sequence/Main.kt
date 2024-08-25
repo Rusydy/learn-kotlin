27a792f30e69585b7a87c960cdf6b5b5c4138b03
Rusydy Muhiddin <rusydymuhiddin@gmail.com>
2024-08-26 02:39:39 +0700

    solve: Alphabetical Sequence

---
+++ Alphabetical Sequence/Main.kt
@@ -0,0 +1,29 @@
+/*
+In this kata you will be given a random string of letters and tasked with returning them as a string of comma-separated sequences sorted alphabetically, with each sequence starting with an uppercase character followed by n-1 lowercase characters, where n is the letter's alphabet position 1-26.
+
+Example:
+alphaSeq("ZpglnRxqenU") -> "Eeeee,Ggggggg,Llllllllllll,Nnnnnnnnnnnnnn,Nnnnnnnnnnnnnn,Pppppppppppppppp,Qqqqqqqqqqqqqqqqq,Rrrrrrrrrrrrrrrrrr,Uuuuuuuuuuuuuuuuuuuuu,Xxxxxxxxxxxxxxxxxxxxxxxx,Zzzzzzzzzzzzzzzzzzzzzzzzzz"
+
+Technical Details
+
+- The string will include only letters.
+- The first letter of each sequence is uppercase followed by n-1 lowercase.
+- Each sequence is separated with a comma.
+- Return value needs to be a string.
+*/
+
+fun main() {
+    println("Test 1 passed: ${alphaSeq("ZpglnRxqenU") == "Eeeee,Ggggggg,Llllllllllll,Nnnnnnnnnnnnnn,Nnnnnnnnnnnnnn,Pppppppppppppppp,Qqqqqqqqqqqqqqqqq,Rrrrrrrrrrrrrrrrrr,Uuuuuuuuuuuuuuuuuuuuu,Xxxxxxxxxxxxxxxxxxxxxxxx,Zzzzzzzzzzzzzzzzzzzzzzzzzz"}")
+
+    println("Test 2 passed: ${alphaSeq("NyffsGeyylB") == "Bb,Eeeee,Ffffff,Ffffff,Ggggggg,Llllllllllll,Nnnnnnnnnnnnnn,Sssssssssssssssssss,Yyyyyyyyyyyyyyyyyyyyyyyyy,Yyyyyyyyyyyyyyyyyyyyyyyyy,Yyyyyyyyyyyyyyyyyyyyyyyyy"}")
+
+    println("Test 3 passed: ${alphaSeq("MjtkuBovqrU") == "Bb,Jjjjjjjjjj,Kkkkkkkkkkk,Mmmmmmmmmmmmm,Ooooooooooooooo,Qqqqqqqqqqqqqqqqq,Rrrrrrrrrrrrrrrrrr,Tttttttttttttttttttt,Uuuuuuuuuuuuuuuuuuuuu,Uuuuuuuuuuuuuuuuuuuuu,Vvvvvvvvvvvvvvvvvvvvvv"}")
+}
+
+fun alphaSeq(letters: String): String {
+  val sorted = letters.lowercase().toCharArray().sorted()
+  return sorted.map { char ->
+      val position = char - 'a' + 1
+      char.toString().repeat(position).capitalize()
+  }.joinToString(",")
+}
