d30126cab47b4587b9c347146c740e2f22db2456
Rusydy Muhiddin <rusydymuhiddin@gmail.com>
2024-08-25 19:29:49 +0700

    Enumerable Magic - Does My List Include This?

--- Main.kt
+++ Main.kt
@@ -1,11 +1,16 @@
-// sum of input digit
-// eg. 476 -> 17
+/*
+Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+*/
 
 fun main() {
-  val input = 476 
-  
-  // Convert each character to its numeric value and sum them
-  val result = input.toString().map { it.toString().toInt() }.sum()
-  
-  println(result)
+  val arr = intArrayOf(1, 2, 3, 4, 5)
+  val item = 3
+  println(include(arr, item)) // Output: true
+
+  val itemNotInArray = 6
+  println(include(arr, itemNotInArray)) // Output: false
+}
+
+fun include(arr: IntArray, item: Int): Boolean {
+  return arr.contains(item)
 }
